0d8f640a4f498a1e22b920d0954bd8c32f3571bc
Roch <szaniewski@users.noreply.github.com>
2020-04-27 13:23:57 +0200

    create deck and shuffling :heavy_check_mark:

--- rs/estimation.py
+++ rs/estimation.py
@@ -1,48 +1,48 @@
 import random
 
-cards = {
-    'figur' : 4,
-    'figur_p' : {
-        'ass': 11,
-        'ten': 10,
-        'king': 4,
-        'ober': 3,
-        'unter': 2,
-        'nine': 0
-    },
-    'marriage_p' : {
-        'hearts': 100,
-        'diamonds': 80,
-        'clubs': 60,
-        'pikes': 40
-    }
-}
+class cards:
 
-gamesruls = {
-    'heand': 10,
-    'stos': 4
-}
+    def __init__( self ):
+        self.playerheand = []
+        self.gams_card = []
+        self.cardonstck = []
+        self.cards = {
+            'figur' : 4,
+            'figur_p' : {
+                'ass': 11,
+                'ten': 10,
+                'king': 4,
+                'ober': 3,
+                'unter': 2,
+                'nine': 0
+            },
+            'marriage_p' : {
+                'hearts': 100,
+                'diamonds': 80,
+                'clubs': 60,
+                'pikes': 40
+            }
+        }
+        self.gamesruls = {
+            'heand': 10,
+            'stos': 4
+        }
 
-gams_card = []
-player_one = []
-player_two = []
+    def deck( self ):
+        """
+        The deck
+        create deck fo game
+        """
+        for card in list( self.cards['figur_p'].keys() ):
+            for color in list( self.cards['marriage_p'].keys() ):
+                self.gams_card.append( card + ' ' + color )
+        return self.gams_card
 
-"""
-The deck
-"""
-for card in list(cards['figur_p'].keys()):
-    for color in list(cards['marriage_p'].keys()):
-        gams_card.append( card + ' ' + color )
-
-"""
-First hend cards
-"""
-
-player_one.append( random.choices( gams_card, k=gamesruls['heand'] ) )
-
-for i in player_one[0]:
-    if i in gams_card:
-        gams_card.remove(str(i))
-
-print( gams_card )
-print( len(gams_card) )
+    def shuffling( self ):
+        """
+        First hend cards
+        """
+        random.shuffle( self.gams_card )
+        self.playerheand += [self.gams_card[:self.gamesruls['heand']]]
+        self.playerheand += [self.gams_card[self.gamesruls['heand']:]]
+        self.cardonstck = self.gams_card[self.gamesruls['heand'] * 2:]
