5eb09e8c6213ad67bbb7d595f787b2d08baebd66
Roch <szaniewski@users.noreply.github.com>
2020-04-27 19:42:05 +0200

    add marriage to scoring :heavy_plus_sign:

--- rs/scoring.py
+++ rs/scoring.py
@@ -7,10 +7,19 @@ class point:
         self.stock = []
 
     def cal( self ):
-        for d in self.data[0]:
-            for p in self.cards['figur_p']:
-                if d.find(p) == 0:
-                    self.incards += self.cards['figur_p'][p]
-        self.stock.append( self.incards )
 
+        for i in range(3):
+            for d in self.data[i]:
+                for p in self.cards['figur_p']:
+                    if d.find(p) == 0:
+                        if d.find('king hearts') == 0 and d.find('ober hearts'):
+                            self.incards += 100
+                        elif d.find('king diamonds') == 0 and d.find('ober diamonds'):
+                            self.incards += 80
+                        elif d.find('king clubs') == 0 and d.find('ober clubs'):
+                            self.incards += 60
+                        elif d.find('king pikes') == 0 and d.find('ober pikes'):
+                            self.incards += 40
+                        self.incards += self.cards['figur_p'][p]
+            self.stock.append( self.incards )
         return self.stock
