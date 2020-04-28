64ff7c1f85cc656dfe6ad91cce3db908226ae7f5
Roch <szaniewski@users.noreply.github.com>
2020-04-28 07:04:05 +0200

    fix cal()

--- rs/scoring.py
+++ rs/scoring.py
@@ -12,14 +12,14 @@ class point:
             for d in self.data[i]:
                 for p in self.cards['figur_p']:
                     if d.find(p) == 0:
-                        if d.find('king hearts') == 0 and d.find('ober hearts'):
-                            self.incards += 100
-                        elif d.find('king diamonds') == 0 and d.find('ober diamonds'):
-                            self.incards += 80
-                        elif d.find('king clubs') == 0 and d.find('ober clubs'):
-                            self.incards += 60
-                        elif d.find('king pikes') == 0 and d.find('ober pikes'):
-                            self.incards += 40
                         self.incards += self.cards['figur_p'][p]
+                    if p.find('king hearts') == 0 and p.find('ober hearts'):
+                        self.incards += 100
+                    elif p.find('king diamonds') == 0 and p.find('ober diamonds'):
+                        self.incards += 80
+                    elif p.find('king clubs') == 0 and p.find('ober clubs'):
+                        self.incards += 60
+                    elif p.find('king pikes') == 0 and p.find('ober pikes'):
+                        self.incards += 40
             self.stock.append( self.incards )
         return self.stock
