16b73a7cd1b96f71fbcf255037406d308631aeb8
Roch <szaniewski@users.noreply.github.com>
2020-04-30 06:31:09 +0200

    Fix trump suit  :feelsgood:

--- rs/skirmish.py
+++ rs/skirmish.py
@@ -1,15 +1,15 @@
 class play:
-    def __init__( self, cards, opponents, playernam, *trumps ):
+    def __init__( self, cards, opponents, playernams, *trumps ):
         self.cards = cards
         self.figurspowers = self.cards.get('figur_p')
         self.winer = ''
         self.opponents = opponents
-        self.playernam = playernam
-        self.trumpsuit = trumps
-        self.playerone = self.playernam[0]
+        self.playernams = playernams
+        self.trumpsuit = str(trumps).strip('(').strip(',),)').strip("'")
+        self.playerone = self.playernams[0]
         self.playeronecolor = self.opponents[0].split(' ')[1]
         self.playeronefigur = self.opponents[0].split(' ')[0]
-        self.playertwo = self.playernam[1]
+        self.playertwo = self.playernams[1]
         self.playertwocolor = self.opponents[1].split(' ')[1]
         self.playertwofigur = self.opponents[1].split(' ')[0]
 
@@ -26,12 +26,11 @@ class play:
         return self.winer
 
     def result( self ):
-
         if self.trumpsuit:
-            if self.playeronecolor ==  self.trumpsuit:
+            if self.playeronecolor == self.trumpsuit:
                 self.winer = self.playerone
             elif self.playertwocolor ==  self.trumpsuit:
-                self.cal()
+                self.winer = self.playertwo
         else:
             self.cal()
 
