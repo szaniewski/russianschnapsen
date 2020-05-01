08977875085c6eb6591e95cc06cdadcadfb963fc
Roch <szaniewski@users.noreply.github.com>
2020-05-01 08:29:44 +0200

    Add setup tinker class :muscle:

--- root/gui.py
+++ root/gui.py
@@ -1,11 +1,32 @@
 import rs.game as rs
 import tkinter as tk
 
-window = tk.Tk()
-window.title( "Russian Schnapsen - game" )
-window.geometry('500x500')
-label = tk.Label( window )
+class satup:
+    def __init__( self ):
+        self.window = tk.Tk()
 
-usersnam = tk.Text( window, width = 100, height = 100 )
+    def windowset( self ):
+        self.window.title( 'Russian Schnapsen - game' )
+        self.window.geometry('500x500')
+        self.label = tk.Label( self.window )
 
-tk.mainloop()
+    def usersname( self ):
+        self.windowset()
+        usernames_hedline = tk.StringVar()
+        usernames = tk.Label(self.window, textvariable = usernames_hedline)
+        usernames_hedline.set("Gamer:")
+        usernames.pack()
+        usernames.place(x= 10, y = 10)
+
+        usernames_input = tk.Entry( self.window, width = 50, textvariable = usernames_hedline )
+        usernames_input.pack()
+        usernames_input.place(x = 10, y = 30)
+
+        tk.mainloop()
+
+def main():
+    s = satup()
+    return s.usersname()
+
+if __name__ == "__main__":
+    main()
