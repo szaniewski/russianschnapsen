e5bfef87881f07519d3409290bb7fb6ce60abbc0
Roch <szaniewski@users.noreply.github.com>
2020-05-02 12:06:17 +0200

    GUI start game :muscle:

--- root/gui.py
+++ root/gui.py
@@ -10,6 +10,16 @@ class satup:
         self.window.geometry('500x500')
         self.label = tk.Label( self.window )
 
+    def start_game( self ):
+        self.playername = self.playernames_input.get()
+        if len( self.playername  ) == 0:
+            print( 'No name' )
+        else:
+            print( self.playername )
+            players = [ self.playername , 'Dead' ]
+            self.coregame = rs.cards( players )
+            print( self.coregame.player() )
+
     def usersname( self ):
         self.windowset()
         usernames_hedline = tk.StringVar()
@@ -18,9 +28,13 @@ class satup:
         usernames.pack()
         usernames.place(x= 10, y = 10)
 
-        usernames_input = tk.Entry( self.window, width = 50, textvariable = usernames_hedline )
-        usernames_input.pack()
-        usernames_input.place(x = 10, y = 30)
+        self.playernames_input = tk.Entry( self.window, width = 50 )
+        self.playernames_input.pack()
+        self.playernames_input.place(x = 10, y = 30)
+
+        start_play_btn = tk.Button(self.window, text = "Start game", command = self.start_game ) # tworzę przycisk, który będzie wstawiał datę
+        start_play_btn.pack()
+        start_play_btn.place(x = 10, y = 50)
 
         tk.mainloop()
 
