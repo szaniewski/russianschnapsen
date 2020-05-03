570b12159d5c32d7a5ddbb5eba993423433fbf0f
Roch <szaniewski@users.noreply.github.com>
2020-05-03 11:45:26 +0200

    add the card character :muscle:

--- root/gui/startgme.py
+++ root/gui/startgme.py
@@ -1,18 +1,10 @@
 import tkinter as tk
 import rs.game as rs
+from PIL import ImageTk, Image
 
 class satup:
     def __init__( self ):
         self.playcard = []
-        self.window = tk.Tk()
-
-    def characters( self ):
-        self.king = tk.PhotoImage( file='gui/gfx/characters/king.gif' )
-        return self.king
-
-    def creatcanvas( self ):
-        self.can = tk.Canvas( self.window , width=500, height=500 )
-        tk.Canvas.create_image(50, 10, characters )
 
     def carddela( self ):
         self.playername = self.playernames_input.get()
@@ -25,21 +17,20 @@ class satup:
             self.usernames.destroy()
             self.playernames_input.destroy()
             self.start_play_btn.destroy()
-            self.creatcanvas()
 
-        print( self.playcard[0].get('cards') )
-       # self.cardonheand( self.playcard[0].get('cards') )
+        self.cardonheadn( self.playcard[0].get('cards') )
         return self.playcard
 
     def satrtgame( self ):
 
+        self.window = tk.Tk()
         self.window.title( 'Russian Schnapsen - game' )
-        self.window.geometry('500x500')
+        self.window.geometry('850x500')
         self.label = tk.Label( self.window )
 
         self.usernames_hedline = tk.StringVar()
         self.usernames = tk.Label( self.window, textvariable =self.usernames_hedline )
-        self.usernames_hedline.set("Gamer:")
+        self.usernames_hedline.set('Gamer:')
         self.usernames.pack()
         self.usernames.place(x= 10, y = 10)
 
@@ -47,12 +38,20 @@ class satup:
         self.playernames_input.pack()
         self.playernames_input.place(x = 10, y = 30)
 
-        self.start_play_btn = tk.Button(self.window, text = "Start game", command = self.carddela )
+        self.start_play_btn = tk.Button(self.window, text = 'Start game', command = self.carddela )
         self.start_play_btn.pack()
         self.start_play_btn.place(x = 10, y = 50)
         tk.mainloop()
 
-    def selectedcard( self, cardname):
-        self.selected = cardname
-        print( self.selected )
-        return self.selected
+    def select_card( self, c ):
+        print('selecterd', c)
+
+    def cardonheadn( self, card ):
+        pos_start_x = 10
+        self.character_king = tk.PhotoImage(file='gui/gfx/characters/king.gif')
+
+        for c in card:
+            self.start_play_btn = tk.Button(self.window,  text = c, image=self.character_king, command = lambda:self.select_card( c ),  borderwidth = 0)
+            self.start_play_btn.pack()
+            self.start_play_btn.place(x = pos_start_x, y = 350 )
+            pos_start_x = pos_start_x + 80
